529c1694f8137ca1d51be1b89171b434829bb97a
jason <lemnik@users.noreply.github.com>
2024-10-24 11:35:35 +0100

    fix(release): replaced the BugsnagGradlePlugin publication with the default pluginMaven publication

--- bugsnag-gradle-plugin/build.gradle.kts
+++ bugsnag-gradle-plugin/build.gradle.kts
@@ -93,39 +93,37 @@ publishing {
         }
     }
 
-    publications {
+    afterEvaluate {
         publications {
-            create<MavenPublication>("BugsnagGradlePlugin") {
-                from(components["java"])
-                groupId = "com.bugsnag"
-                afterEvaluate {
-                    artifactId = project.property("artefactId").toString()
-                }
+            publications {
+                getByName<MavenPublication>("pluginMaven") {
+                    groupId = "com.bugsnag"
+                    artifactId = project.property("POM_ARTIFACT_ID").toString()
 
-                pom {
-                    name = project.property("POM_NAME").toString()
-                    description = project.property("POM_DESCRIPTION").toString()
-                    url = project.property("POM_URL").toString()
+                    pom {
+                        name = project.property("POM_NAME").toString()
+                        description = project.property("POM_DESCRIPTION").toString()
+                        url = project.property("POM_URL").toString()
 
-                    licenses {
-                        license {
-                            name = project.property("POM_LICENCE_NAME")?.toString()
-                            url = project.property("POM_LICENCE_URL")?.toString()
-                            description = project.property("POM_LICENCE_DIST")?.toString()
+                        licenses {
+                            license {
+                                name = project.property("POM_LICENCE_NAME")?.toString()
+                                url = project.property("POM_LICENCE_URL")?.toString()
+                            }
                         }
-                    }
 
-                    developers {
-                        developer {
-                            id = project.property("POM_DEVELOPER_ID")?.toString()
-                            name = project.property("POM_DEVELOPER_NAME")?.toString()
+                        developers {
+                            developer {
+                                id = project.property("POM_DEVELOPER_ID")?.toString()
+                                name = project.property("POM_DEVELOPER_NAME")?.toString()
+                            }
                         }
-                    }
 
-                    scm {
-                        connection = project.property("POM_SCM_CONNECTION")?.toString()
-                        developerConnection = project.property("POM_SCM_DEV_CONNECTION")?.toString()
-                        url = project.property("POM_SCM_URL")?.toString()
+                        scm {
+                            connection = project.property("POM_SCM_CONNECTION")?.toString()
+                            developerConnection = project.property("POM_SCM_DEV_CONNECTION")?.toString()
+                            url = project.property("POM_SCM_URL")?.toString()
+                        }
                     }
                 }
             }
@@ -133,16 +131,13 @@ publishing {
     }
 }
 
-signing {
-    sign(publishing.publications["BugsnagGradlePlugin"])
-}
-
-tasks.named("publishBugsnagGradlePluginPublicationToMavenRepository") {
-    dependsOn("signPluginMavenPublication")
-}
-
 afterEvaluate {
-    tasks.named("publishPluginMavenPublicationToMavenRepository") {
-        dependsOn("signBugsnagGradlePluginPublication")
+    signing {
+        sign(publishing.publications["pluginMaven"])
     }
+}
+
+// Workaround for https://github.com/gradle/gradle/issues/15568
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    mustRunAfter(tasks.withType<Sign>())
 }
