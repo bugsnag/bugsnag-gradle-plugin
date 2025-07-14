9f9c826e7bf3d23dd2f9227aaff94d0709cf0464
Josh <j.edney17@gmail.com>
2025-07-14 16:02:02 +0100

    add publish script and buildkite step

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("java-gradle-plugin")
     id("com.github.hierynomus.license") version "0.16.1" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.9.20" apply false
     id("io.gitlab.arturbosch.detekt") version "1.23.5" apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.3.1" apply false
     id("com.gradle.plugin-publish") version "1.2.1" apply false
