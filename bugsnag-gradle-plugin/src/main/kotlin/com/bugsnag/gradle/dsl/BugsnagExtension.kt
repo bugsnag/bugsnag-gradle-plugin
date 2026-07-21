f825b3c6020419f9b37a2608118abe7f88824757
SB-sachinp <sachin.patil@smartbear.com>
2026-07-21 12:38:19 +0530

    Build kite fix.

--- bugsnag-gradle-plugin/src/main/kotlin/com/bugsnag/gradle/dsl/BugsnagExtension.kt
+++ bugsnag-gradle-plugin/src/main/kotlin/com/bugsnag/gradle/dsl/BugsnagExtension.kt
@@ -12,8 +12,10 @@ open class BugsnagExtension @Inject constructor(objects: ObjectFactory) : Bugsna
     override var timeout: Int? = null
     override var retries: Int? = null
     override var apiKey: String? = null
+
     @Transient
     override var buildUuidGenerator: (() -> String)? = null
+
     override var buildUuid: String? = null
     override var versionNameOverride: String? = null
     override var versionCodeOverride: Int? = null
